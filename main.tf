677f22cca28ded96b888d24ec6bd3dabea730af4
Mallika <kmallika.malli@gmail.com>
2023-09-14 12:07:04 +0530

    creating auto scaling group

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_launch_template" "template" {
   vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
-resource "aws_autoscaling_group" "bar" {
+resource "aws_autoscaling_group" "asg" {
   name   = "${var.name}-${var.env}-asg"
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
