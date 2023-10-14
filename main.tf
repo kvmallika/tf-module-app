82c04e9d0f933b6d0ff69312e34a60495034415f
Mallika <kmallika.malli@gmail.com>
2023-10-14 16:36:02 +0530

    added listener and target group

--- main.tf
+++ main.tf
@@ -41,6 +41,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.template.id
@@ -54,4 +55,10 @@ resource "aws_autoscaling_group" "asg" {
       value               = tag.value
     }
   }
+}
+resource "aws_lb_target_group" "main" {
+  name     = "${var.name}-alb-${var.env}"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
 }
