349ebffac7f4f6fdcd12846deed4cc14439b0dec
Mallika <kmallika.malli@gmail.com>
2023-10-17 23:44:54 +0530

    added parameters in iam.tf

--- locals.tf
+++ locals.tf
@@ -3,5 +3,5 @@ locals {
   dns_name = "${var.dns_name}.${var.domain_name}"
 
   parameters = concat(var.parameters, [var.name])
-  resources = [ for parameters in local.parameters: "arn:aws:ssm:us-east-1:${data.aws_caller_identity.identity.account_id}:parameter/${var.env}.${var.name}.*" ]
+  resources = [ for parameter in local.parameters: "arn:aws:ssm:us-east-1:${data.aws_caller_identity.identity.account_id}:parameter/${var.env}.${parameter}.*" ]
 }
