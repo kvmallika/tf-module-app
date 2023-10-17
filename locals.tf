9301c292f668c75b01576ad6f8b4c3a498a8605e
Mallika <kmallika.malli@gmail.com>
2023-10-17 23:37:36 +0530

    added parameters in iam.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,7 @@
 locals {
   asg_tags =  merge(var.tags, { Name = "${var.name}-${var.env}" })
   dns_name = "${var.dns_name}.${var.domain_name}"
+
+  parameters = concat(var.parameters, [var.name])
+  resources = [ for parameters in local.parameters: "arn:aws:ssm:us-east-1:${data.aws_caller_identity.identity.account_id}:parameter/${var.env}.${var.name}.*" ]
 }
