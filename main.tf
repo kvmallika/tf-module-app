aed30768d1bbfff105e03094cd31b817c0990f3f
Mallika <kmallika.malli@gmail.com>
2023-11-01 11:34:09 +0530

    added monitor_cidr variable

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "sg" {
     protocol         = "tcp"
     cidr_blocks      = var.bastion_cidr_block
   }
+  ingress {
+    description      = "PROMETHEUS"
+    from_port        = 9100
+    to_port          = 9100
+    protocol         = "tcp"
+    cidr_blocks      = var.monitor_cidr
+  }
 
   egress {
     from_port        = 0
